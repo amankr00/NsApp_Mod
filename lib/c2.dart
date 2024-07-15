de80cfc663645e0ac6663ce481580a97c107f351
Aman Kumar <93372156+amankr00@users.noreply.github.com>
2024-07-15 17:04:09 +0530

    17th Commit

--- lib/c2.dart
+++ lib/c2.dart
@@ -37,7 +37,7 @@ class _KaamState extends State<Kaam> {
             child: Column(
               children: [
                 SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 Text('ZILA PARISHAD NALANDA',
                     style: TextStyle(
@@ -45,10 +45,51 @@ class _KaamState extends State<Kaam> {
                         fontSize: 50,
                         fontFamily: 'poppins',
                         decoration: TextDecoration.none)),
+                        SizedBox(height: 20,),
+               
                 SizedBox(
-                  height: 60,
+                  height: 15,
+                ),
+                Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color : Color.fromARGB(255, 210, 210, 210),
+                ),
+                width: wid*0.45,
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   Text('Financial Head : XYZ',
+                      style: TextStyle(
+                          // color:textColor,
+                          color:Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'lato1',
+                          decoration: TextDecoration.none)),
+                          SizedBox(width: 40,),
+                  Text('Financial Year : XYZ',
+                      style: TextStyle(
+                          color:Colors.black,
+                          // color:textColor,
+                          fontSize: 20,
+                          fontFamily: 'lato1',
+                          decoration: TextDecoration.none)),
+                          SizedBox(width: 40,),
+                  Text('Scheme Name : XYZ',
+                      style: TextStyle(
+                          // color:textColor,
+                          color:Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'lato1',
+                          decoration: TextDecoration.none,
+                          fontWeight:FontWeight.normal)),
+                          // SizedBox(width: 20,),
+                  ],),
                 ),
 
+                SizedBox(
+                  height: 30,
+                ),
                 // for(int i=0;i<4;i++)
                 
                 roc(hei, wid),
@@ -91,7 +132,7 @@ class _KaamState extends State<Kaam> {
           children: [
             chip("Stage 1", wid, hei ,textColor , background),
             chip("Good", wid, hei,textColor, background),
-            chip("Bad", wid, hei,textColor, background),
+            chip("Baad", wid, hei,textColor, background),
             chip("Worst", wid, hei,textColor, background),
           ],
         ),
@@ -131,7 +172,7 @@ class _KaamState extends State<Kaam> {
           children: [
             chip("Stage 1", wid, hei ,textColor , background),
             chip("Good", wid, hei,textColor, background),
-            chip("Bad", wid, hei,textColor, background),
+            chip("Baad", wid, hei,textColor, background),
             chip("Worst", wid, hei,textColor, background),
           ],
         ),
@@ -145,7 +186,7 @@ class _KaamState extends State<Kaam> {
         //       ),
         //     )),
          Positioned(
-         top:28,
+         top:29,
          left: 65,
          child:
          SemicircleWidget1(radius: 116, strokeWidth: 4, color: textColor,)),
@@ -199,7 +240,8 @@ class _KaamState extends State<Kaam> {
                     decoration: TextDecoration.none,
                     fontWeight: FontWeight.w400)),
             
-            Container(
+            Padding(padding: EdgeInsets.only(left : 15),
+            child : Container(
             width: MediaQuery.of(context).size.width / 12 ,
             // height: MediaQuery.of(context).size.height / 3,
               child: Text(
@@ -210,6 +252,7 @@ class _KaamState extends State<Kaam> {
                       color:textColor,
                       decoration: TextDecoration.none,
                       fontWeight: FontWeight.normal)),
+            )
             ),
           ],
             
